eb9f6b849e70a51cf7c56e3e1331c17fb59da970
jonthan roten <jonathanroten@icloud.com>
2025-02-04 18:44:11 -0500

    Installed Swiper JS
    1. Installed Swiper Packeage
    2. Created articles-swiper.ts
    3. Imported basic Swiper markup.

---
+++ src/utils/articles-swiper.ts
@@ -0,0 +1,9 @@
+import 'swiper/css';
+
+import Swiper from 'swiper';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+});
