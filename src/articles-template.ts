3627020866e33d362a4dbe0a4e09c5ebd8d26b08
jonthan roten <jonathanroten@icloud.com>
2025-03-06 13:53:54 -0500

    Created initReadTime
    1. Created utility read-time.ts
    2. imported to article-templates.ts

--- src/articles-template.ts
+++ src/articles-template.ts
@@ -1,6 +1,8 @@
 import { initArticlesSwiper } from '$utils/articles-swiper';
+import { initReadTime } from '$utils/read-time';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   initArticlesSwiper();
+  initReadTime();
 });
---
+++ src/utils/read-time.ts
@@ -0,0 +1,12 @@
+export const initReadTime = () => {
+  const source = document.querySelector('[data-readTime-element="source"]') as HTMLElement;
+  const target = document.querySelector('[data-readTime-element="target"]') as HTMLElement;
+  if (!source || !target) return;
+
+  const WORDSPERMINUTE = 225;
+
+  const text = source.innerText;
+  const words = text.trim().split(/\s+/).length;
+  const estimate = Math.ceil(words / WORDSPERMINUTE);
+  target.innerText = `${estimate.toString()} min read`;
+};
