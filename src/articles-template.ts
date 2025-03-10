955c7a385cdf33d4f698c8c88b157e3357901a7a
jonthan roten <jonathanroten@icloud.com>
2025-03-10 09:34:15 -0400

    edited card animation

--- src/articles-template.ts
+++ src/articles-template.ts
@@ -1,10 +1,8 @@
 import { initArticlesSwiper } from '$utils/articles-swiper';
-//import { initCardAnimation } from '$utils/card-animation';
 import { initReadTime } from '$utils/read-time';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   initArticlesSwiper();
   initReadTime();
-  // initCardAnimation();
 });
