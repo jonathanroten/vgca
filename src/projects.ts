5e4c2a358de58fddfe38730bc2017cb389aa2e56
jonthan roten <jonathanroten@icloud.com>
2025-04-03 18:27:49 -0400

    updated featured project script

--- src/projects.ts
+++ src/projects.ts
@@ -1,8 +1,10 @@
 import { initBackgroundAnimation } from '$utils/gsap-background';
 import { initFeaturedProjects } from '$utils/gsap-featured-projects';
+import { initStickyPanels } from '$utils/gsap-sticky';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   initBackgroundAnimation();
   initFeaturedProjects();
+  initStickyPanels();
 });
--- src/utils/gsap-featured-projects.ts
+++ src/utils/gsap-featured-projects.ts
@@ -5,43 +5,25 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const initFeaturedProjects = () => {
   const featuredProjects = document.querySelectorAll('[data-animation-element="featured-project"]');
-  /*const featuredProjectsContent = document.querySelectorAll(
-    '[data-animation-element="featured-project-content"]'
-  );*/
-  const stickyTop = getComputedStyle(document.documentElement).getPropertyValue('--site--margin');
 
-  if (featuredProjects.length > 0) {
-    featuredProjects.forEach((prjoect, index) => {
-      (prjoect as HTMLElement).style.position = 'sticky';
-      (prjoect as HTMLElement).style.top = stickyTop;
-
-      const isLast = index === featuredProjects.length - 1;
-      const featuredProjectAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: prjoect,
-          start: 'top top',
-          scrub: 1,
-        },
-      });
-      // .set(featuredProjectsContent, { opacity: 1 });
-
-      featuredProjectAnimation.to(
-        prjoect,
-        {
-          ease: 'none',
-          //startAt: { filter: 'blur(0px)' },
-          //filter: isLast ? 'blur(0px)' : 'blur(5px)',
-          scale: isLast ? 1 : 0.9,
-        },
-        '<'
-      );
-      /*.to(
-          featuredProjectsContent,
-          {
-            opacity: isLast ? 1 : 0,
-          },
-          '<'
-        );*/
+  featuredProjects?.forEach((prjoect) => {
+    const featuredProjectAnimation = gsap.timeline({
+      scrollTrigger: {
+        markers: true,
+        trigger: prjoect,
+        start: 'top top',
+        scrub: true,
+        anticipatePin: 1,
+      },
     });
-  }
+
+    featuredProjectAnimation.to(
+      prjoect,
+      {
+        ease: 'none',
+        scale: 0.9,
+      },
+      '<'
+    );
+  });
 };
