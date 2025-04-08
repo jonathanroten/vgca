e50fc74589888c41902c11249c5659be9adca1c8
jonthan roten <jonathanroten@icloud.com>
2025-04-08 11:23:37 -0400

    Update gsap-featured-projects

--- src/utils/gsap-featured-projects.ts
+++ src/utils/gsap-featured-projects.ts
@@ -5,6 +5,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const initFeaturedProjects = () => {
   const featuredProjects = document.querySelectorAll('[data-animation-element="featured-project"]');
+  const featuredBanner = document.querySelector(
+    '[data-animation-element="featured-banner"]'
+  ) as HTMLElement;
 
   featuredProjects?.forEach((project) => {
     const featuredProjectAnimation = gsap.timeline({
@@ -21,5 +24,15 @@ export const initFeaturedProjects = () => {
       autoAlpha: 0,
       ease: 'none',
     });
+  });
+  gsap.to(featuredBanner, {
+    autoAlpha: 0,
+    duration: 0.2,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: featuredBanner,
+      start: 'top top',
+      toggleActions: 'play none none reverse',
+    },
   });
 };
