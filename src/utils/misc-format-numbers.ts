a35be29ab498191aca28586f0071a6472f9212a9
jonthan roten <jonathanroten@icloud.com>
2025-04-08 11:01:41 -0400

    Update misc-format-number

--- src/utils/misc-format-numbers.ts
+++ src/utils/misc-format-numbers.ts
@@ -1,13 +1,12 @@
 export const initFormatNumber = () => {
   // Select all elements that have the number
-  const numbers = document.querySelectorAll('[data-format-number]');
+  const numbers = document.querySelectorAll('[data-format-number="true"]');
   numbers.forEach((el) => {
     const currentNumber = el as HTMLElement;
     const formatedNumber = parseInt(currentNumber.textContent || '0', 10);
     if (!isNaN(formatedNumber)) {
       currentNumber.textContent =
         formatedNumber < 10 ? '0' + formatedNumber : formatedNumber.toString();
-      console.log('Formatted number:', currentNumber.textContent);
     }
   });
 };
