53b1bd632abaef6e9c58ca78ccd4eaa8ea5c0c42
jonthan roten <jonathanroten@icloud.com>
2025-04-10 09:49:00 -0400

    Update projects-template.ts
    Included sticky panels

--- src/projects-template.ts
+++ src/projects-template.ts
@@ -1,6 +1,8 @@
+import { initStickyPanels } from '$utils/gsap-sticky';
 import { initWorkSwiper } from '$utils/swiper-related-work';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   initWorkSwiper();
+  initStickyPanels();
 });
