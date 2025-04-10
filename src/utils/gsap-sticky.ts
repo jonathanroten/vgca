f19045ab99c7259cb7b2f2e78da8894d875f678a
jonthan roten <jonathanroten@icloud.com>
2025-04-10 13:19:58 -0400

    added media query to gsap-stikcy.ts
    Media query set's animation for desktop-only

--- src/utils/gsap-sticky.ts
+++ src/utils/gsap-sticky.ts
@@ -5,18 +5,21 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const initStickyPanels = () => {
   const stickyPanels = document.querySelectorAll('[data-animation-element="sticky-panel"]');
+  const mm = gsap.matchMedia();
 
-  if (stickyPanels.length > 0) {
-    stickyPanels.forEach((panel) => {
-      const sickyPanelAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: panel,
-          start: 'bottom -50%',
-          scrub: true,
-        },
+  mm.add('(min-width: 992px)', () => {
+    if (stickyPanels.length > 0) {
+      stickyPanels.forEach((panel) => {
+        const sickyPanelAnimation = gsap.timeline({
+          scrollTrigger: {
+            trigger: panel,
+            start: 'bottom -50%',
+            scrub: true,
+          },
+        });
+
+        sickyPanelAnimation.set(panel, { opacity: 1 }).to(panel, { opacity: 0, duration: 0.1 });
       });
-
-      sickyPanelAnimation.set(panel, { opacity: 1 }).to(panel, { opacity: 0, duration: 0.1 });
-    });
-  }
+    }
+  });
 };
