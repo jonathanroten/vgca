1d5de581aca32ee4e52e0f1fb1763461c7d7cf17
jonthan roten <95926661+jonathanroten@users.noreply.github.com>
2025-09-28 11:31:28 -0400

    Update swiper-articles.ts

--- src/utils/swiper-articles.ts
+++ src/utils/swiper-articles.ts
@@ -5,22 +5,25 @@ export const initArticlesSwiper = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const swiper = new Swiper('[data-swiper-element="articles-instance"]', {
     modules: [Navigation],
-    spaceBetween: 24,
-    slidesPerView: 1,
+    slidesPerView: 'auto',
+    followFinger: true,
+    loopAdditionalSlides: 10,
+    freeMode: false,
+    slideToClickedSlide: true,
+    centeredSlides: false,
+    autoHeight: false,
     speed: 300,
+    mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
     navigation: {
       nextEl: '[data-swiper-element="next"]',
       prevEl: '[data-swiper-element="prev"]',
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 16,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 16,
-      },
     },
   });
 };
